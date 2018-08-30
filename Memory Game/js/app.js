4a07a6166011fb9a93ce0f39ee1f75a2b22d2816
GUZHAO <guzhao1990@gmail.com>
2018-08-30 12:59:44 -0400

    upload

---
+++ Memory Game/js/app.js
@@ -0,0 +1,89 @@
+/*
+ * Create a list that holds all of your cards
+ */
+let card = document.getElementsByClassName('card');
+// Convert to array for shuffle function use
+let cardList = [...card];
+console.log(cardList[1].children);
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+const shuffledDeck = shuffle(cardList);
+const addDeck = document.querySelector('.deck');
+for (let aCard of shuffledDeck) {
+    addDeck.appendChild(aCard);
+}
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+for (let aCard of shuffledDeck) {
+    aCard.addEventListener('click', flipCard);
+}
+
+function flipCard() {
+    this.classList.toggle('open');
+    this.classList.toggle('show');
+}
+
+function openCard() {
+    let openedCard = [];
+    openedCard.push(this);
+    console.log(openedCard);
+    if (openedCard.length === 2) {
+        if (openedCard[0].children === openedCard[1].children) {
+            matchedCards();
+        }
+        else {
+            unmatchedCards();
+        }
+    }
+    else {
+    }
+    countMove();
+}
+
+function matchedCards() {
+
+}
+
+function unmatchedCards() {
+
+}
+
+function countMove() {
+
+}
+
+function score() {
+
+}
