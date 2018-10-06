8cb9e589b5b5014cc11f9c1473c972a0c7092958
GUZHAO <guzhao1990@gmail.com>
2018-10-06 12:17:09 -0400

    updated arcade game project

---
+++ Arcade Game/README.md
@@ -0,0 +1,15 @@
+Arcade Game
+===============================
+
+## Quick Start
+The goal of this game is reach to the water area which is the top part of the panel. 
+The player starts at the bottom center and can use direction keys (up, right, down and left) to control the character.
+There are 3 enemies int he game and they are always roaming.
+
+## Rule
+* Player can only roam around within the panel
+* Enemies can roam at different speed
+* If the character collides with any enemy it will go back to the starting point
+* When player reaches to the water area then the player wins and will go back to the starting point
+
+
---
+++ Arcade Game/js/app.js
@@ -0,0 +1,104 @@
+// Enemies our player must avoid
+
+const Enemy = function (x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function (dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += dt * this.speed;
+    // Handles collision with the Player
+    if (player.x < this.x + 90 &&
+        player.y < this.y + 60 &&
+        player.x + 90 > this.x &&
+        player.y + 60 > this.y) {
+        player.x = 202;
+        player.y = 405;
+    }
+    // Enemy speed changes after first move
+    if (this.x > 510) {
+        this.x = -100;
+        this.speed = 100 + Math.random() * 120
+    }
+
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+const Player = function (x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function () {
+
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function (key) {
+    // Implement direction keys and they will only work within the game panel
+    if (key === 'left' && this.x > 0) {
+        this.x -= 101;
+    }
+    if (key === 'up' && this.y > 0) {
+        this.y -= 83;
+    }
+    if (key === 'right' && this.x < 404) {
+        this.x += 101;
+    }
+    if (key === 'down' && this.y < 405) {
+        this.y += 83;
+    }
+    if (this.y < 0) {
+        setTimeout(() => {
+            this.x = 202;
+            this.y = 405;
+        }, 800);
+    }
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+const allEnemies = [];
+// Generate 3 enemies on each road
+[60, 145, 228].forEach(function (loc) {
+    const enemy = new Enemy(0, loc, 200);
+    allEnemies.push(enemy);
+});
+
+// Place the player object in a variable called player
+const player = new Player(202, 405);
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function (e) {
+    const allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
