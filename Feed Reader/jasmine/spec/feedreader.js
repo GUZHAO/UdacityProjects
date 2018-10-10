e386125bc7df97d63dccbd4eb1a6c9a08c5b1885
GUZHAO <guzhao1990@gmail.com>
2018-10-10 11:46:54 -0400

    updated feedreader

--- Feed Reader/jasmine/spec/feedreader.js
+++ Feed Reader/jasmine/spec/feedreader.js
@@ -27,8 +27,8 @@ $(function () {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
+        /* loops through each feed
+         * in the allFeeds object and ensures it has a URL and name defined
          * and that the URL is not empty.
          */
         it('has URL(s)', () => {
@@ -38,10 +38,6 @@ $(function () {
             })
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
         it('has name(s)', () => {
             allFeeds.forEach(function (element) {
                 expect(element.name).toBeDefined();
@@ -53,7 +49,7 @@ $(function () {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', () => {
-        /* TODO: Write a test that ensures the menu element is
+        /* ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -61,7 +57,7 @@ $(function () {
         it('is hidden', () => {
             expect(document.body.classList.contains('menu-hidden')).toEqual(true);
         });
-        /* TODO: Write a test that ensures the menu changes
+        /* ensures the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
